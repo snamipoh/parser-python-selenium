1dccb4cedce80b1bc3beca3194512dd2035561cf
Dmitry Becker <jupyter@tuta.io>
2020-06-21 14:34:04 +0300

    Refactored complex if or statement into a loop

--- evolutiongaming.py
+++ evolutiongaming.py
@@ -11,10 +11,8 @@ chromedriver = "C:\\chromedriver.exe"
 driver = webdriver.Chrome(chromedriver)
 driver.get("https://www.evolutiongamingcareers.com/search-jobs/?department=&country=")
 
-print('Scrolling down the page')
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-print('Click element to download vacancies')
 while True:
     try:
         WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='more-jobs-button']"))).click()
@@ -35,7 +33,6 @@ job_date = str(date.today())
 num_values = len(titles)
 
 for i in range(num_values):
-
     jobs.append({
         'title': titles[i].text,
         'link': links[i].get_attribute("href"),
@@ -45,22 +42,30 @@ for i in range(num_values):
         'job_date': job_date
     })
 
-job_type2position = (
-    (['javascript', 'typescript', 'react', 'angular', 'vue', 'front'],  'Frontend'),
+# print(jobs)
+
+# Deleting unnecessary words after parsing locations
+for job in jobs:
+    index_number = job["location"].find(" ")
+    # print(index_number)
+    job["location"] = job["location"][index_number + 1:]
+
+job_type_position = (
+    (['javascript', 'typescript', 'react', 'angular', 'vue', 'front'], 'Frontend'),
     (['scala'], 'Scala'),
     (['devops'], 'DevOps'),
     (['product owner'], 'Product'),
     (['qa'], 'QA'),
-    (["data analyst", "database developer", "data center engineer", "big data", "analyst", "data scientist", "bi analyst" ,"data engineer", "business intelligence" ,"business analyst"], "Data & BI"),
+    (["data analyst", "database developer", "data center engineer", "big data", "analyst", "data scientist", "bi analyst", "data engineer", "business intelligence", "business analyst"], "Data & BI"),
     (["sre", "site reliability engineer"], 'SRE'),
     (["security", "security engineer"], 'Security'),
     (["groovy", "groovy engineer"], "Groovy"),
     (["embedded software developer", "embedded software engineer", "embedded"], "Embedded SW"),
-    (["ux/ui designer", "UX researcher", "designer", "ux designer"], 'Design'),
-    (["backend", "back"], "Backend"),
+    (["ux/ui designer", "ux researcher", "designer", "ux designer"], 'Design'),
+    (["backend", "back"], "Backend")
 )
 
-map2country = {
+map_country = {
     "netherlands": "Netherlands",
     "malta": "Malta",
     "romania": "Romania",
@@ -70,22 +75,22 @@ map2country = {
     "estonia": "Estonia"
  }
 
+
 def get_job_type(title):
-    for job_titles, job_type in job_type2position:
+    for job_titles, job_type in job_type_position:
         for job_title in job_titles:
             if job_title in title:
                 return job_type
     return 'Other'
 
+
 for element in jobs:
-    print('Getting job type')
     title = element['title'].lower()
     job_type = get_job_type(title)
     element['job_type'] = job_type
 
-    print('Processing location')
     location = element['location'].lower()
-    location_clean = map2country.get(location, 'Other')
+    location_clean = map_country.get(location, 'Other')
     element['location'] = location_clean
 
 # print(jobs)
@@ -94,8 +99,10 @@ for element in jobs:
 tuples = [tuple(x.values())[0:] for x in jobs]
 
 print(tuples)
+print(len(tuples))
 
 # Uploading parsed records to the DB
+
 conn = sqlite3.connect("jobs.db")
 cursor = conn.cursor()
 
@@ -104,6 +111,5 @@ cursor.executemany("INSERT INTO jobs (job_title, job_link, job_location, job_typ
 
 conn.commit()
 conn.close()
-
 
 driver.quit()
