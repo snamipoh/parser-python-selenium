38442f0c2d56e407d67a07a86223a30d35cda9dd
Dmitry Becker <jupyter@tuta.io>
2020-06-21 17:57:16 +0300

    Refactored complex if or statement into a loop

--- personio.py
+++ personio.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from datetime import date
-import time
 import sqlite3
 
 
@@ -27,48 +26,62 @@ jobs = []
 section = driver.find_elements_by_xpath("//div[@id='job-board']//div")
 
 for i in section:
-
     jobs.append({
-        'job_title': i.find_element_by_css_selector("p.h3").text,
-        'job_link': i.find_element_by_css_selector("a.personio_yellow_button_medium").get_attribute("href"),
-        'job_location': i.find_element_by_css_selector("p.w-10").text,
+        'title': i.find_element_by_css_selector("p.h3").text,
+        'link': i.find_element_by_css_selector("a.personio_yellow_button_medium").get_attribute("href"),
+        'location': i.find_element_by_css_selector("p.w-10").text,
         'job_type': "Other",
         'job_company': "personio",
         'job_date': str(date.today())
     })
 
-    for element in jobs:
-        if "frontend" in element['job_title'].lower() or "front" in element['job_title'].lower():
-            element['job_type'] = "Frontend"
-        elif "backend" in element['job_title'].lower() or "back" in element['job_title'].lower():
-            element['job_type'] = "Backend"
-        elif "data analyst" in element['job_title'].lower() or "bi analyst" in element['job_title'].lower() \
-                or "data engineer" in element['job_title'].lower() \
-                or "business intelligence" in element['job_title'].lower():
-            element['job_type'] = "Data & BI"
-        elif "engineering manager" in element['job_title'].lower() \
-                or "head of infrastructure" in element['job_title'].lower() \
-                or "director of technology" in element['job_title'].lower() \
-                or "director of product" in element['job_title'].lower():
-            element['job_type'] = "Tech Lead"
-        else:
-            element['job_type'] = "Other"
+# print(jobs)
+
+job_type_position = (
+    (["devops"], "DevOps"),
+    (["data analyst", "bi analyst", "data engineer", "business intelligence"], "Data & BI"),
+    (["product manager", "product marketing"], "Product"),
+    (["designer"], "Design"),
+    (["backend", "back"], "Backend"),
+    (["frontend", "front", "javascript", "typescript"], "Frontend"),
+    (["fullstack", "full stack", "full-stack"], "Fullstack"),
+    (["security", "security engineer"], 'Security'),
+    (["ux/ui designer", "ux researcher", "designer", "ux designer"], 'Design'),
+    (["engineering manager", "head of infrastructure", "director of technology", "director of product"], "Tech Lead")
+)
+
+map_country = {
+    "munich": "Germany",
+    "madrid": "Spain"
+}
+
+
+def get_job_type(title):
+    for job_titles, job_type in job_type_position:
+        for job_title in job_titles:
+            if job_title in title:
+                return job_type
+    return 'Other'
+
 
 for element in jobs:
-    if "munich" in element['job_location'].lower():
-        element['job_location'] = "Germany"
-    elif "madrid" in element['job_location'].lower():
-        element['job_location'] = "Spain"
-    else:
-        element['job_location'] = "Other"
+    title = element['title'].lower()
+    job_type = get_job_type(title)
+    element['job_type'] = job_type
+
+    location = element['location'].lower()
+    location_clean = map_country.get(location, 'Other')
+    element['location'] = location_clean
 
 # print(jobs)
 
 # Converting dict into list of tuples
 tuples = [tuple(x.values())[0:] for x in jobs]
 print(tuples)
+print(len(tuples))
 
 # Uploading parsed records to the DB
+
 conn = sqlite3.connect("jobs.db")
 cursor = conn.cursor()
 
